16b83ac1e958e5e2875857a33d0e3f5a9e9036ae
Jesse Abarca <JesseAbarca888@gmail.com>
2023-09-28 00:35:34 -0700

    Configure body-parser to read the body of the POST request sent from index.html

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(morgan('dev'));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
