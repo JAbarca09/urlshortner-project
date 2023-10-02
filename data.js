26660a5a0e5cd90830209d02cf5554d12e85f37b
Jesse Abarca <JesseAbarca888@gmail.com>
2023-10-01 22:54:25 -0700

    Create a data.js file that handles saving and reading data to db

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const dbFilePath = path.join(__dirname, 'urls.json');
 
-function readData(callback) {
+exports.readData = (callback) => {
   fs.readFile(dbFilePath, 'utf8', (err, data) => {
     if (!err) {
       try {
@@ -18,17 +18,35 @@ function readData(callback) {
   });
 }
 
-function writeData(data, callback) {
-  fs.writeFile(dbFilePath, JSON.stringify(data), (writeErr) => {
-    if (writeErr) {
-      callback(writeErr);
-    } else {
-      callback(null);
-    }
-  });
-}
+// TODO
+// Write a function that takes an object {original_url: x} and save it to the "backend"
+// Generate a random six digit unique number for the shorturl and save it to urls.json w/ function
 
-module.exports = {
-  readData,
-  writeData,
+exports.writeData = (newData, callback) => {
+  fs.readFile(dbFilePath, 'utf-8', (readErr, data) => {
+    if (readErr) {
+      console.error('Error reading file:', readErr);
+      callback({ error: 'Internal server error' });
+      return;
+    }
+
+    const urlData = JSON.parse(data);
+
+    urlData.push(newData);
+
+    fs.writeFile(
+      dbFilePath,
+      JSON.stringify(urlData, null, 2),
+      'utf-8',
+      (writeErr) => {
+        if (writeErr) {
+          console.error('Error writing file:', writeErr);
+          callback({ error: 'Internal server error' });
+          return;
+        }
+        callback(null, newData);
+      }
+    );
+  });
 };
+
